d7e2e5b692eb1d1005525e6e26426ee94de32fcd
worldOneo <62426504+worldOneo@users.noreply.github.com>
2020-09-24 21:59:14 +0200

    Added default rout

--- main.go
+++ main.go
@@ -18,12 +18,13 @@ const configFilePath = "muxcdn.conf.json"
 
 // Config The default cdn config
 type Config struct {
-	Addr      string   `json:"addr"`
-	Workdir   string   `json:"workdir"`
-	Whitelist []string `json:"whitelist"`
-	TLS       bool     `json:"TLS"`
-	CertFile  string   `json:"certfile"`
-	KeyFile   string   `json:"keyfile"`
+	DefaultFile string   `json:"defaultfil"`
+	Addr        string   `json:"addr"`
+	Workdir     string   `json:"workdir"`
+	Whitelist   []string `json:"whitelist"`
+	TLS         bool     `json:"TLS"`
+	CertFile    string   `json:"certfile"`
+	KeyFile     string   `json:"keyfile"`
 }
 
 // Server the server which contains every information needed
@@ -62,6 +63,7 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/{file:.+}", deliverContent)
+	r.HandleFunc("/", deliverDefault)
 
 	if server.config.TLS {
 		fmt.Println("Starting Server with SSL!")
@@ -103,13 +105,15 @@ func loadConfig() (*Config, error) {
 	defer configFile.Close()
 	if err != nil {
 		config := Config{
-			Addr:      "0.0.0.0:8080",
-			Workdir:   ".",
-			Whitelist: []string{"/var/www/html/*"},
-			TLS:       false,
-			KeyFile:   "key.pem",
-			CertFile:  "key.crt",
+			DefaultFile: "index.html",
+			Addr:        "0.0.0.0:8080",
+			Workdir:     ".",
+			Whitelist:   []string{"/var/www/html/*"},
+			TLS:         false,
+			KeyFile:     "key.pem",
+			CertFile:    "key.crt",
 		}
+
 		data, jsonErr := json.MarshalIndent(config, "", "    ")
 		if jsonErr != nil {
 			return nil, jsonErr
@@ -147,9 +151,33 @@ func deliverContent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data, err := readContent(indirName, server.cache)
+	err := sendFile(w, r, indirName)
+
+	if err != nil {
+		logOut(fmt.Sprintf("ERR [%v] 404", err), r, false)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 page not found"))
+	}
+}
+
+func deliverDefault(w http.ResponseWriter, r *http.Request) {
+	logOut(fmt.Sprintf("REQ [%s]", "/"), r, true)
+
+	err := sendFile(w, r, server.config.DefaultFile)
+
+	if err != nil {
+		logOut(fmt.Sprintf("ERR [%v] 404", err), r, false)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 page not found"))
+	}
+}
+
+func sendFile(w http.ResponseWriter, r *http.Request, file string) error {
+	data, err := readContent(file, server.cache)
 	if err == nil {
-		endings := strings.Split(f, ".")
+		endings := strings.Split(file, ".")
 		if len(endings) > 0 {
 			ending := endings[len(endings)-1]
 			contenttype, exists := TypeMap[ending]
@@ -162,13 +190,8 @@ func deliverContent(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "text/html")
 		}
 		w.WriteHeader(http.StatusOK)
-		logOut(fmt.Sprintf("RES [%s] 200", f), r, false)
+		logOut(fmt.Sprintf("RES [%s] 200", file), r, false)
 		w.Write(data)
-		return
 	}
-
-	logOut(fmt.Sprintf("ERR [%v] 404", err), r, false)
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("404 page not found"))
+	return err
 }
